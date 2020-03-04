29b43d288b10ac48c98144af19fc3817aae74406
Felipe Menanteau <felipe@illinois.edu>
2020-03-04 11:39:02 -0600

    Changes for ts_xml 4.7

--- docker/build.sh
+++ docker/build.sh
@@ -1,7 +1,13 @@
 export LSSTTS_SALOBJ_VERSION=5.4.0
 export LSSTTS_XML_VERSION=4.7.0
+export LSSTTS_SALIDL_VERSION=1.1.2
 export HEADERSERVICE_VERSION=1.4.0
 export DDS_DOMAIN=auxtelpath
-export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_xml_v${LSSTTS_XML_VERSION}
+export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_idl_v${LSSTTS_SALIDL_VERSION}_xml_v${LSSTTS_XML_VERSION}
 
-docker build . -t lsstts/at_headerservice:$TAG --build-arg LSSTTS_SALOBJ_VERSION --build-arg LSSTTS_XML_VERSION --build-arg HEADERSERVICE_VERSION --build-arg DDS_DOMAIN
+docker build . -t lsstts/at_headerservice:$TAG \
+       --build-arg LSSTTS_SALIDL_VERSION \
+       --build-arg LSSTTS_SALOBJ_VERSION \
+       --build-arg LSSTTS_XML_VERSION \
+       --build-arg HEADERSERVICE_VERSION \
+       --build-arg DDS_DOMAIN
