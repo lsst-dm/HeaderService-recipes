0925bb98b7df8182d8912d2566b02956ae1391f2
Felipe Menanteau <felipe@illinois.edu>
2020-04-25 17:09:39 -0500

    Updates to new versions

--- conda/build_conda_steps.sh
+++ conda/build_conda_steps.sh
@@ -1,3 +1,9 @@
-export HEADERSERVICE_VERSION=1.4.1
-export LSSTTS_SALOBJ_VERSION=v5.4.0
+export HEADERSERVICE_VERSION=2.0.0
+export LSSTTS_SALOBJ_VERSION=5.10.0
 conda-build . -c conda-forge -c lsstts
+echo " "
+echo "--------------------"
+echo "Now Please upload:"
+echo " anaconda upload --force -u lsstts $CONDA_PREFIX/conda-bld/linux-64/headerservice-$HEADERSERVICE_VERSION-py37_0.tar.bz2"
+echo " "
+echo "--------------------"
