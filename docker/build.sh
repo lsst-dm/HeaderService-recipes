57da674cabed4e3fdc40b7b49cbde63dd9c8f1a4
Felipe Menanteau <felipe@illinois.edu>
2020-06-19 10:10:30 -0500

    New idl/salobj/xml versions

--- docker/build.sh
+++ docker/build.sh
@@ -1,13 +1,13 @@
-export LSSTTS_SALOBJ_VERSION=5.14.0
-export LSSTTS_XML_VERSION=5.1.0
-export LSSTTS_SALIDL_VERSION=1.2.0
+export LSSTTS_SALOBJ_VERSION=5.15.0
+export LSSTTS_XML_VERSION=6.0.0
+export LSSTTS_SALIDL_VERSION=1.2.2
 export HEADERSERVICE_VERSION=2.3.0
-#export DDS_DOMAIN=auxtelpath
 export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_idl_v${LSSTTS_SALIDL_VERSION}_xml_v${LSSTTS_XML_VERSION}
 
 docker build . -t lsstts/headerservice:$TAG \
        --build-arg LSSTTS_SALIDL_VERSION \
        --build-arg LSSTTS_SALOBJ_VERSION \
        --build-arg LSSTTS_XML_VERSION \
+       --build-arg LSSTTS_SAL_VERSION \
        --build-arg HEADERSERVICE_VERSION \
        --build-arg DDS_DOMAIN
