c58823babd494ab238dfbf43f66f32374edda1cd
Felipe Menanteau <felipe@illinois.edu>
2020-11-05 12:46:49 -0600

    Update for new xml 6.2.1

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,9 @@
-export LSSTTS_SALOBJ_VERSION=6.0.2
-export LSSTTS_XML_VERSION=6.1.0
-export LSSTTS_SALIDL_VERSION=2.0.0
-export HEADERSERVICE_VERSION=2.5.0
-export LSSTTS_DEPLOY_VERSION=c0014
+export LSSTTS_SALOBJ_VERSION=6.0.3
+export LSSTTS_XML_VERSION=6.2.1
+export LSSTTS_SALIDL_VERSION=2.1.0
+export LSSTTS_DDSCONFIG_VERSION=v0.1.0
+export HEADERSERVICE_VERSION=2.5.1
+export LSSTTS_DEPLOY_VERSION=c0015
 export TAG=v${HEADERSERVICE_VERSION}_salobj_v${LSSTTS_SALOBJ_VERSION}_idl_v${LSSTTS_SALIDL_VERSION}_xml_v${LSSTTS_XML_VERSION}
 
 docker login ts-dockerhub.lsst.org
@@ -12,5 +13,6 @@ docker build . -t ts-dockerhub.lsst.org/headerservice:ts-$TAG \
        --build-arg LSSTTS_SALOBJ_VERSION \
        --build-arg LSSTTS_XML_VERSION \
        --build-arg LSSTTS_DEPLOY_VERSION\
+       --build-arg LSSTTS_DDSCONFIG_VERSION\
        --build-arg HEADERSERVICE_VERSION \
        --build-arg DDS_DOMAIN
